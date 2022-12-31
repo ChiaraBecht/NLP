4e4308915f5db80d0c6d02466faec8e958dd2fea
ChiaraBecht <c.becht@st.hanze.nl>
2022-12-31 12:07:41 +0100

    code to load my own corpus from csv pipe delimited files

---
+++ embedding_based_Confidence_classification/Import_own_corpus_flair.py
@@ -0,0 +1,24 @@
+import flair
+print(flair.__version__)
+
+# load own corpus
+from flair.data import Corpus
+from flair.datasets import CSVClassificationCorpus
+from pathlib import Path
+
+# this is the folder in which train, test and dev files reside
+data_folder = r'C:\\Data_Science_for_Life_Sciences_MASTER\\masterthesis\\Quota_workaround\\NLP\\embedding_based_Confidence_classification\\test_corpus'
+#print(data_folder)
+column_name_map = {0: "text", 1:"label"}  
+label_type = "label" 
+
+corpus = CSVClassificationCorpus(data_folder, 
+                                        column_name_map, 
+                                        skip_header=True, 
+                                        delimiter="|",
+                                        #train_file= 'train.csv',
+                                        dev_file= 'dev.csv',
+                                        #test_file= 'test.csv', 
+                                        label_type=label_type)
+
+print(corpus.get_all_sentences()[0])
